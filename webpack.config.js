b6cfc36a0f5e5cc4a7ef84f869b2a339c6e5fc09
josipaTomic <57952742+josipaTomic@users.noreply.github.com>
2020-03-11 15:24:30 +0100

    Fixed tipos

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,10 @@ module.exports=(env)=>{
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true
+            historyApiFallback: true,
+            publicPath: '/dist/'
+
+
         }
         
     };
